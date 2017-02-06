39667b0ff8b3c760ebe4dc1c5d016331387843b2
Sumit kushwa <sumitkushwahji@gmail.com>
2017-02-06 18:54:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/:articleName',function (req, res){
 var counter=0;
 app.get('/counter', function (req, res) {
     counter=counter+1;
-    res.send(counter.toString);
+    res.send(counter.toString());
 });
 
 
