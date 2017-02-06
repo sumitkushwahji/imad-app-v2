0accb36203704b73dec063af8352b6b810c8aba4
Sumit kushwa <sumitkushwahji@gmail.com>
2017-02-06 16:39:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="new Page";
+//move image
+var img=document.getElementById('madi');
+img.onClick= function(){
+    img.style.marginleft='100px';
+};
