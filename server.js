029daa25ad0c781074391ec41ed659b35149709b
Sumit kushwa <sumitkushwahji@gmail.com>
2017-02-06 13:00:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,61 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var articleOne={
+    title:'article-one | sumit kushwah',
+    heading: 'Article-one',
+    date: '5 sep 2017',
+    content:`<p>
+my name is sumie kusheahajjvd hjdbhbc jhbchbshbcns j s sjh hsd h s js jhs jsd sjhdh js jh sdd
+</p>
+<p>
+my name is sumie kusheahajjvd hjdbhbc jhbchbshbcns j s sjh hsd h s js jhs jsd sjhdh js jh sdd
+</p>
+
+<p>
+my name is sumie kusheahajjvd hjdbhbc jhbchbshbcns j s sjh hsd h s js jhs jsd sjhdh js jh sdd
+</p>`
+};
+
+
+function createTemplate(data){ 
+    var title= data.title;
+    var date= data.date;
+    var heading= data.heading;
+    var content= data.content;
+    
+    var htmlTemplate = `<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name= "viewport" content= "width-device=width , initial-scale=1"/>
+       <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+      <div class="container">  <div>
+         <a href="/">HOME</a>
+         </div>
+         <hr/>
+         <h3>
+           ${heading}
+             </h3>
+         <div>
+        <p3>
+       ${date}
+        </p3>
+        </div>
+    ${content}
+        </div>
+    </body>
+</html>`;
+return htmlTemplate;
+}
+
+
+
 app.get('/article-one',function (req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+  res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function (req, res){
