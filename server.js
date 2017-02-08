f697fc6000b31d555b193d734969429e7d078d29
Sumit kushwa <sumitkushwahji@gmail.com>
2017-02-09 01:28:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,16 +90,20 @@ function createTemplate(data){
 return htmlTemplate;
 }
 
-app.get('/:articleName',function (req, res){
-  var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
-});
+
 
 var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
+
+app.get('/:articleName',function (req, res){
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
+
+
 
 
 app.get('/ui/style.css', function (req, res) {
