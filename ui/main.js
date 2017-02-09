60847442719a633b71618d6f372d0e658883bbd6
Sumit kushwa <sumitkushwahji@gmail.com>
2017-02-09 20:55:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,14 +30,31 @@ var nameInput= document.getElementById('name');
 var name = nameInput.value;
 var submit =document.getElementById('submit_btn');
 submit.onclick = function(){
-    //make a request to the server and send the name
     
-    //capture a list of name and render it as a list
-    var names=['name1','name2','name3','name4'];
+    //create a request object
+    var request =new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange= function () {
+    if (request.readyState === XMLHttpRequest.DONE){
+        
+        //Take the action
+        if(request.status === 200){
+    var names= JSON.parse(names);
     var list='';
     for(var i=0;i< names.length; i++){
         list += '<li>'+ names[i] + '</li>';
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML = list;
+        }
+    }
+    //make a request to the server and send the name
+      request.open('GET', 'http://sumitkushwahji.imad.hasura-app.io/submit-name?name=' + name, true);
+   request.send(null);
+    
+    
+    
+    //capture a list of name and render it as a list
+   
 };
