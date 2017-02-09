81653a4f001ba1930a83b296f26b2b21b1308602
Sumit kushwa <sumitkushwahji@gmail.com>
2017-02-09 20:32:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,8 +117,8 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
-app.get('/submit-name/:name',function(req, res ){
+var names= [];
+app.get('/submit-name/:name',function(req, res){
     //get the name from the request
     var name=req.params.name;
     
