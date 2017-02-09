1a0d19ebe43e7b362b21e4609d3705ff62d91b6f
Sumit kushwa <sumitkushwahji@gmail.com>
2017-02-09 20:44:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,6 +98,18 @@ app.get('/counter', function (req, res) {
     res.send(counter.toString());
 });
 
+var names= [];
+app.get('/submit-name',function(req, res)  //make request query parameters /submit-name?name=sumit
+{
+    //get the name from the request
+    var name=req.query.name;
+    
+    names.push(name);
+    //JSON: javascript OBject notation
+    res.send(JSON.stringify(names));
+    
+});
+
 app.get('/:articleName',function (req, res){
   var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
@@ -118,17 +130,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names= [];
-app.get('/submit-name',function(req, res)  //make request query parameters /submit-name?name=sumit
-{
-    //get the name from the request
-    var name=req.query.name;
-    
-    names.push(name);
-    //JSON: javascript OBject notation
-    res.send(JSON.stringify(names));
-    
-});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
