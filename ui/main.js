5fc744bda774200ced495eda389e5225e041499f
Sumit kushwa <sumitkushwahji@gmail.com>
2017-02-10 00:53:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -61,3 +61,39 @@ var name = nameInput.value;
     //capture a list of name and render it as a list
    
 };
+  
+  //submit comment
+  var submit =document.getElementById('comment_btn1');
+submit.onclick = function(){
+    
+    //create a request object
+    var request =new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    request.onreadystatechange= function () {
+    if (request.readyState === XMLHttpRequest.DONE){
+        
+        //Take the action
+        if(request.status === 200){
+       var names= request.responseText;
+    names = JSON.parse(names);
+    var list='';
+    for(var i=0;i< names.length; i++){
+        list += '<li>'+ names[i] + '</li>';
+    }
+    var ul=document.getElementById('commentlist1');
+    ul.innerHTML = list;
+        }
+    }
+    };
+    //make a request to the server and send the name
+    var nameInput= document.getElementById('comment_box1');
+var name = nameInput.value;
+      request.open('GET', 'http://sumitkushwahji.imad.hasura-app.io/submit-name?name=' + name, true);
+   request.send(null);
+    
+    
+    
+    //capture a list of name and render it as a list
+   
+};
