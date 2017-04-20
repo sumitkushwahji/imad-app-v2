fcddcd6b924c229dcb339e8737f69b5ef66b9ef5
Sumit kushwa <sumitkushwahji@gmail.com>
2017-04-20 23:36:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -337,7 +337,7 @@ function createTemplate2(data){
 			<li><a href="Projects">Projects</a></li>
 			<li><a href="Training">Training</a></li>
 			<li><a href="Certificate">Certification</a></li>
-			<li><a href="/">Positions</a></li>
+			<li><a href="Education">Education</a></li>
 			<li><a href="/">Students</a></li>
 			<li><a href="/">Contact</a><li>
 		</ul>
